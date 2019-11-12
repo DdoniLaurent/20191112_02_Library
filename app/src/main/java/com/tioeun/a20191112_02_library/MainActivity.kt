daeba9a2083a7b2f2d976b9d5de4337d8a5d9613
YongMinSun <ddoni1010@gmail.com>
2019-11-12 22:04:38 +0900

    글라이드로 이미지 넣기 예제

--- app/src/main/java/com/tioeun/a20191112_02_library/MainActivity.kt
+++ app/src/main/java/com/tioeun/a20191112_02_library/MainActivity.kt
@@ -2,6 +2,8 @@ package com.tioeun.a20191112_02_library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseAcivity() {
 
@@ -19,6 +21,7 @@ class MainActivity : BaseAcivity() {
     }
 
     override fun setValues() {
-
+        var url = "https://dispatch.cdnser.be/wp-content/uploads/2018/05/20180501224056_j.jpg"
+        Glide.with(this).load(url).into(profileImg)
     }
 }
